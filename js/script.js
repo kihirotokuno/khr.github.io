c271c24d8d54eeb0c1a5c22871261a0fc56d7eb5
khr <kihiro.tokuno@gmail.com>
2024-01-20 20:20:33 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -58,7 +58,16 @@ loader.load(
 
 		console.log( 'An error happened' );
 
-	}
+    },
 
+    function animate() {
+        requestAnimationFrame( animate );
 
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render( scene, camera );
+    }
 );
+
+animate();
