6ba6cc83bf4017d786a7d54cdaafda6a0677a253
khr <kihiro.tokuno@gmail.com>
2024-01-20 20:16:14 +0900

    added cube

--- js/script.js
+++ js/script.js
@@ -10,11 +10,11 @@ camera.position.z = 1;
 
 const scene = new THREE.Scene();
 
-// const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-// const material = new THREE.MeshNormalMaterial();
+const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+const material = new THREE.MeshNormalMaterial();
 
-// const mesh = new THREE.Mesh( geometry, material );
-// scene.add( mesh );
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setSize( width, height );
