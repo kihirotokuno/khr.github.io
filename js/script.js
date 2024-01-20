b2b0f38cf81d82081682c7a0693957ea1bcdfbbc
khr <kihiro.tokuno@gmail.com>
2024-01-21 01:20:13 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -107,7 +107,37 @@ const loader = new GLTFLoader();
 loader.load(
 	// resource URL
 	'./assets/3dmodel/pc.gltf',
-    './assets/3dmodel/icon_3d.gltf',
+
+	// called when the resource is loaded
+	function ( gltf ) {
+
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+    },
+);
+
+loader.load(
+	// resource URL
+	'./assets/3dmodel/icon_3d.gltf',
+
 	// called when the resource is loaded
 	function ( gltf ) {
 
