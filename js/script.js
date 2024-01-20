b905ed51dcfdaf265cae527964a76d0afd6a1bed
khr <kihiro.tokuno@gmail.com>
2024-01-20 19:43:17 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,11 +10,11 @@ camera.position.z = 1;
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
+// const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+// const material = new THREE.MeshNormalMaterial();
 
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
+// const mesh = new THREE.Mesh( geometry, material );
+// scene.add( mesh );
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setSize( width, height );
@@ -36,11 +36,11 @@ loader.load( './Assets/3dmodel/pc.glb', function ( gltf ) {
 
 // animation
 
-function animation( time ) {
+// function animation( time ) {
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+// 	mesh.rotation.x = time / 2000;
+// 	mesh.rotation.y = time / 1000;
 
-	renderer.render( scene, camera );
+// 	renderer.render( scene, camera );
 
-}
+// }
