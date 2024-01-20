882491eee20643e5d25981b73a91e33bb5f4aabf
khr <kihiro.tokuno@gmail.com>
2024-01-21 00:24:44 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,10 +5,9 @@ const width = window.innerWidth, height = window.innerHeight;
 
 // init
 
-const camera = new THREE.OrthographicCamera(1, width / height, 0.01, 1000 );
+const camera = new THREE.PerspectiveCamera(17, width / height, 0.01, 1000 );
 //camera.position.z = 1;
-camera.position.set(0, 0, -5);
-camera.rotation.set(0, 180, 0);
+camera.position.set(0, 0, 2);
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xDAFF00 );
