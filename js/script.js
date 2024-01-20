97720d1d897a961eede69466abd5f1825d7fb285
khr <kihiro.tokuno@gmail.com>
2024-01-20 18:58:26 +0900

    added threejs master + updated path

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
     //1/20
     // Scene and renderer setup
+
+
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
