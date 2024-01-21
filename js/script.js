55389398e7cf5e2c143477591d4217e4228c5d1a
khr <kihiro.tokuno@gmail.com>
2024-01-22 00:22:40 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -183,7 +183,8 @@ const material = new THREE.PointsMaterial({
 plyLoader.load('./assets/3dmodel/idd_lab.ply', (geometry) => { //引数にはpositionとcolorを持つBufferGeometryが入ってる
     const particles = new THREE.Points(geometry, material_pc);
     particles.rotation.set(0, 90, 90);
-    particles.size.set(0.1, 0.1, 0.1);
+    particles.scale.set(0.1, 0.1, 0.1);
+
     scene.add(particles);
 });
 
